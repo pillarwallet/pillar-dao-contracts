4a534bc1b572337d47fc8441c02a79dbddd4e947
lbw33 <71776468+lbw33@users.noreply.github.com>
2023-06-14 15:09:09 +0100

    Added deployment and setup script for PillarDAO contracts.

---
+++ contracts/helpers/TestToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestToken is ERC20, Ownable {
+    constructor() ERC20("TestToken", "TEST") {
+        _mint(msg.sender, 100000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
